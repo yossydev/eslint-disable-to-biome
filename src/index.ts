2f6868be95d8072279b2fa98ae7b2c72ab39e37c
Yuto Yoshino <codeyy021@gmail.com>
2024-07-20 13:40:08 +0900

    feat: cli (#2)
    * feat: cli
    
    * chore: dependencies ts-morph

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { Project } from "ts-morph";
+import { Command } from "commander";
+const program = new Command();
 
-const project = new Project({
-	tsConfigFilePath: "./tsconfig.json",
-});
-const sourceFiles = project.getSourceFiles();
+program
+	.name("eslint-disable-to-biome")
+	.description(
+		"A cli tool that allows eslint disable comments that are left behind when migrating from eslint to biome to be transferred to biome.",
+	)
+	.version("0.1.0");
+
+program.parse();
