031b9ff7b0aae973da650133fa69d9d3fa5bb9ec
yossydev <codeyy021@gmail.com>
2024-07-20 14:01:20 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { Command } from "commander";
+import { Project } from "ts-morph";
+import { migrateExhaustiveDepsComment } from "./lib/migrate-exhaustive-deps-comment.js";
+import { glob } from "glob";
+
 const program = new Command();
 
 program
@@ -6,6 +10,28 @@ program
 	.description(
 		"A cli tool that allows eslint disable comments that are left behind when migrating from eslint to biome to be transferred to biome.",
 	)
-	.version("0.1.0");
+	.version("0.1.0")
+	.option(
+		"--ext <extensions...>",
+		"File extensions to include (default: ts,tsx)",
+		"ts,tsx",
+	)
+	.argument("[glob]", "Glob pattern to match files")
+	.action((globPattern, options) => {
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		const extensions = options.ext[0].split(",").map((ext: any) => ext.trim());
+		const files = glob.sync(globPattern, { nodir: true });
+
+		const project = new Project();
+
+		for (const file of files) {
+			if (extensions.includes(file.split(".").pop())) {
+				const sourceFile = project.addSourceFileAtPath(file);
+				migrateExhaustiveDepsComment(sourceFile);
+			}
+		}
+
+		console.log("Migration completed.");
+	});
 
 program.parse();
